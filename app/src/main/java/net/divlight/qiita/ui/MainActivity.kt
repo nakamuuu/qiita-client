112ce7f93c1fb373f982f8bd8340fd6f6e5f519d
nakamuuu <keita111.mpgnt700.programming@gmail.com>
2017-05-28 15:53:50 +0900

    Add a search options menu in MainActivity

--- app/src/main/java/net/divlight/qiita/ui/MainActivity.kt
+++ app/src/main/java/net/divlight/qiita/ui/MainActivity.kt
@@ -2,6 +2,9 @@ package net.divlight.qiita.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import net.divlight.qiita.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +14,22 @@ class MainActivity : AppCompatActivity() {
                     .replace(android.R.id.content, ItemFragment.newInstance())
                     .commit()
         }
+    }
+
+    //
+    // Options Menu
+    //
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean = when (item?.itemId) {
+        R.id.search -> {
+            // TODO: start SearchActivity
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 }
